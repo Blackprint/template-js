eff2a84018f4dc4e9cf8984a6f4799c1dc360a59
StefansArya <stefansarya1@gmail.com>
2021-11-21 20:13:10 +0700

    Improve some configuration

--- blackprint.config.js
+++ blackprint.config.js
@@ -3,8 +3,8 @@ module.exports = {
 	name: "Empty Template",
 	description: "Your awesome nodes",
 
-	// This must be set to false if you want to compile this nodes
-	disabled: true,
+	// If this set to true, the compiler will skip this config/module
+	disabled: false,
 
 	// Uncomment this if you want to copy /dist/... files to current folder
 	// Especially if you want to publish it to NPM Registry
@@ -21,13 +21,13 @@ module.exports = {
 	// Maybe you want to write Node.js compatible node on ".js"
 	// and browser compatible node on ".sf" file extension
 	js:{
-		file:'@cwd/dist/nodes-yours.js', // @cwd = directory where you start the Node.js
+		file:'@cwd/dist/nodes-yours.mjs', // @cwd = directory where you start the Node.js
 
 		// We're not using `await imports.task()` so let's just use `mjs`
 		wrapped: 'mjs', // Wrap the entire .js to .mjs
 
 		combine:[ // Relative to this config's directory
-			'src/_init.js', // First rule = first priority
+			'src/_init.js', // Rule order/index may have different priority
 			'src/**/*.js',
 		],
 	},
@@ -35,13 +35,13 @@ module.exports = {
 	// This extension can contain html, scss, and js
 	// But only use this if you only develop for browser API
 	sf:{
-		file:'@cwd/dist/nodes-yours.sf', // will have sf.css and sf.js
+		file:'@cwd/dist/nodes-yours.sf', // will have sf.css and sf.mjs
 
 		// Use `async-mjs` if we want to use `await imports.task()` to avoid waiting this module
 		wrapped: 'async-mjs', // Wrap the entire .js in async IIFE to .mjs file
 
 		combine:[ // Relative to this config's directory
-			'src/_init.sf', // First rule = first priority
+			'src/_init.sf', // Rule order/index may have different priority
 			'src/**/*.sf',
 		],
 	}
